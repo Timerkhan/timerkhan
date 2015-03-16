a94a13aec762e1cb065eeb5b558e99204164dd98
Timerkhan <timerkhan-94@mail.ru>
2015-03-16 18:37:04 +0200

    Update reclass.java

--- src/reclass.java
+++ src/reclass.java
@@ -1,14 +1,11 @@
-public class reclass {
-    public static String GetNumberStandart(String a) {
-        a = a.toLowerCase();
-        String result = a.replaceAll("\\s|\\.", "");
-        return  result;
-
+public class Reclass {
+    public static String getnumberstandart(String a) {
+        return a.toLowerCase().replaceAll("\\s|\\.", "");
     }
     public static void main(String[] args) throws java.lang.Exception {
         {
             String n = "wasD.12as 2e daDv.sW";
-            System.out.println(GetNumberStandart(n));
+            System.out.println(getnumberstandart(n));
         }
 
     }
